f821606b0657eaa9674c7a1d8d224b246de9f8d6
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2015-05-19 08:36:22 -0400

    basic put call that is not completely dadaistic

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate hyper;
 
 use std::io::Read;
 use std::string::String;
+use std::vec::Vec;
 
 use protobuf::parse_from_bytes;
 use protobuf::Message;
@@ -18,11 +19,6 @@ mod api;
 mod config;
 mod data;
 mod errors;
-
-struct Call {
-    args: Box<protobuf::Message>,
-    reply: Box<protobuf::Message>,
-}
 
 trait Request {
     fn mut_header(&mut self) -> &mut api::RequestHeader;
@@ -47,5 +43,42 @@ fn main() {
     let mut putArgs = api::PutRequest::new();
     let mut putResp = putArgs.reply();
     putArgs.mut_header().set_raft_id(1);
-    println!("result: {}", putResp.mut_header().has_error());
+    putArgs.mut_header().set_user("root".to_owned());
+    putArgs.mut_header().set_key(b"tkey".to_vec());
+
+    let mut sender = Client::new();
+
+    let e = sender.send(
+        &putArgs,
+        &mut putResp,
+    );
+
+    println!("{}", putResp.get_header().get_timestamp().get_wall_time());
+}
+
+trait KVSender {
+    fn send(&mut self, &Message, &mut Message);
+}
+
+impl KVSender for Client {
+    fn send(&mut self, args: &Message, reply: &mut Message) {
+        let enc = args.write_to_bytes().unwrap();
+        //reply.merge_from_bytes(&enc);
+
+        let mut headers = Headers::new();
+        headers.set(ContentType("application/x-protobuf".parse().unwrap()));
+        let res = self.post("http://localhost:8080/kv/db/Put")
+            .body(&*enc) // or &enc[..]
+            .headers(headers)
+            .send();
+        match res {
+            Ok(mut resp) => match resp.status {
+                StatusCode::Ok => {
+                    let proto_res = reply.merge_from(&mut protobuf::CodedInputStream::new(&mut resp));
+                },
+                _ => {},
+            },
+            _ => {},
+        }
+    }
 }
